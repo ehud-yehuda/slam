2d185a240ddf546d81ef05c747abce03f266477a
d <d>
2022-01-31 10:17:28 +0200

    adding feature extractor class

--- main.py
+++ main.py
@@ -4,13 +4,44 @@ from Display import Display
 
 
 W = 2160 // 2
-H = 3840 // 2
+H = 3840 // 4
+
+class FeatueExtractor:
+    GX = 9
+    GY = 8
+    def __init__(self):
+        self.orb = cv2.ORB_create(100)
+        pass
+
+    def extract(self, img):
+        kp_all = []
+        sy = img.shape[0] // self.GY
+        sx = img.shape[1] // self.GX
+
+        for ry in range(0, img.shape[0], sy):
+            for rx in range(0, img.shape[1], sx):
+                _img = img[ry:ry+sy, rx:rx+sx, :]
+                kp = self.orb.detect(_img, None)
+                for p in kp:
+                    p.pt = (p.pt[0] + rx, p.pt[1] + ry)
+                    kp_all.append(p)
+                    self._drawKpImg(p, img)
+
+    def _drawKpImg(self, kp, img):
+        u, v = map(lambda x: int(round(x)), kp.pt)
+        cv2.circle(img, (u, v), color=(0, 255, 0), radius=3)
+        return
+
 
 disp = Display(w=W, h=H)
+fe = FeatueExtractor()
 
 def process_image(img):
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.resize(img, (W, H))
+    fe.extract(img)
+
+    # draw only keypoints location,not size and orientation
+    # img = cv2.drawKeypoints(img, kp, None, color=(0, 255, 0), flags=0)
     disp.draw(img)
 
 def run():
